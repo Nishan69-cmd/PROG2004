fde1ef15614d104675cbea3ac45b4f07e24c916a
Nishan69-cmd <nishanbanskota9@gmail.com>
2026-07-22 23:57:54 +0800

    completed HealthProfessional

--- HealthProfessional.java
+++ HealthProfessional.java
@@ -25,4 +25,24 @@ public abstract class HealthProfessional {
     public String[] getWorkingDays() {
         return workingDays;
     }
+
+    public abstract String getProfessionalType();
+
+    @Override
+    public String toString() {
+        String days = "";
+
+        for (int i = 0; i < workingDays.length; i++) {
+            days = days + workingDays[i];
+
+            if (i < workingDays.length - 1) {
+                days = days + ", ";
+            }
+        }
+
+        return "Professional Type: " + getProfessionalType()
+                + "\nProfessional ID: " + professionalID
+                + "\nName: " + name
+                + "\nWorking Days: " + days;
+    }
 }

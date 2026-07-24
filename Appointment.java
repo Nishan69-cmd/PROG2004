82118f9e9c15a68e71c902c15f4542e6445297ec
Nishan69-cmd <nishanbanskota9@gmail.com>
2026-07-24 23:25:28 +0800

    created Appointment and tested aswell

---
+++ Appointment.java
@@ -0,0 +1,40 @@
+public class Appointment implements Comparable<Appointment> {
+
+    private Patient patient;
+    private HealthProfessional healthProfessional;
+    private String appointmentTime;
+
+    public Appointment(
+            Patient patient,
+            HealthProfessional healthProfessional,
+            String appointmentTime) {
+
+        this.patient = patient;
+        this.healthProfessional = healthProfessional;
+        this.appointmentTime = appointmentTime;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public HealthProfessional getHealthProfessional() {
+        return healthProfessional;
+    }
+
+    public String getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    @Override
+    public int compareTo(Appointment other) {
+        return this.appointmentTime.compareTo(other.appointmentTime);
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment Time: " + appointmentTime
+                + "\n" + patient
+                + "\n" + healthProfessional;
+    }
+}

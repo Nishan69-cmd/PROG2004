7608cc5fbec32701458849c1a05103d3630f670c
Nishan69-cmd <nishanbanskota9@gmail.com>
2026-07-24 20:36:39 +0800

    created Dietitian

---
+++ Dietitian.java
@@ -0,0 +1,29 @@
+public class Dietitian extends HealthProfessional {
+
+    private String nutritionArea;
+
+    public Dietitian(
+            int professionalID,
+            String name,
+            String[] workingDays,
+            String nutritionArea) {
+
+        super(professionalID, name, workingDays);
+        this.nutritionArea = nutritionArea;
+    }
+
+    public String getNutritionArea() {
+        return nutritionArea;
+    }
+
+    @Override
+    public String getProfessionalType() {
+        return "Dietitian";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()
+                + "\nNutrition Area: " + nutritionArea;
+    }
+}

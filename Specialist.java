b8108226b0ce7f4470045c90a2cfd91a57906543
Nishan69-cmd <nishanbanskota9@gmail.com>
2026-07-26 18:32:20 +0800

    completed adding comments

--- Specialist.java
+++ Specialist.java
@@ -1,7 +1,8 @@
+//represent specialist
 public class Specialist extends HealthProfessional {
 
     private String specialty;
-
+//creates specialist with professionalid name working days specialty
     public Specialist(
             int professionalID,
             String name,
@@ -11,16 +12,16 @@ public class Specialist extends HealthProfessional {
         super(professionalID, name, workingDays);
         this.specialty = specialty;
     }
-
+//returns speciality
     public String getSpecialty() {
         return specialty;
     }
-
+//returns professionaltype
     @Override
     public String getProfessionalType() {
         return "Specialist";
     }
-
+//returns detail as readable text
     @Override
     public String toString() {
         return super.toString()
